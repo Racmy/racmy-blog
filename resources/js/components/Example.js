57872a8f2a02c156ed98b34776f09c1183ac1617
Tomonori Matsukawa <maccho.chekera@gmail.com>
2019-03-24 13:07:21 +0900

    記事投稿画面のスタイルの調整

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -15,7 +15,6 @@ export default class Example extends React.Component {
         this.getCurrentContent = this.getCurrentContent.bind(this);
         this.myBlockStyleFn = this.myBlockStyleFn.bind(this);
         this.onToggle = this.onToggle.bind(this)
-        // キーバンド
         this.handleKeyCommand = this.handleKeyCommand.bind(this);
     }
     onChange (editorState) {
@@ -49,27 +48,79 @@ export default class Example extends React.Component {
         return false;
     }
     render() {
-        const styles = ['BOLD', 'ITALIC', 'UNDERLINE', 'CODE'];
-        const inlineButtons = styles.map(style => {
-            return <button key={style} onClick={this.onClick} name={style}>{style}</button>
+        const styles = {
+            bold: 'BOLD',
+            italic: 'ITALIC',
+            underline: 'UNDERLINE',
+            code: 'CODE'
+        };
+        const inlineButtons = Object.keys(styles).map(style => {
+            return (<button key={style} onClick={this.onClick} name={styles[style]} className="btn page-link text-dark d-inline-block rounded-0">
+                        <i className={"fas fa-" + style}></i>
+                    </button>)
         })
-        const elements = ['unstyled', 'code-block', 'atomic', 'blockquote', 'header-one', 'header-two', 'header-three', 'header-four', 'header-five', 'header-six', 'unordered-list-item', 'ordered-list-item']
-        const eleButtons = elements.map(element => {
-            return <button key={element} onClick={this.onToggle} name={element} class="btn page-link text-dark d-inline-block rounded-0">{element}</button>
+        const elements = {
+            div: 'unstyled',
+            code: 'code-block',
+            h1: 'header-one',
+            h2: 'header-two',
+            h3: 'header-three',
+            h4: 'header-four',
+            h5: 'header-five',
+            h6: 'header-six',
+            ul: 'unordered-list-item',
+            ol: 'ordered-list-item'
+        }
+        const elementButtons = Object.keys(elements).map(tagName => {
+            return <button key={tagName} onClick={this.onToggle} name={elements[tagName]} className="btn page-link text-dark d-inline-block rounded-0">{tagName}</button>
         })
-        const saveButton = (
-            <button onClick={this.saveEditor}>保存</button>
+        const saveButtons = (
+            <div>
+                <button onClick={this.saveEditor} className="btn page-link text-dark d-inline-block rounded-0">投稿</button>
+                <button onClick={this.saveEditor} className="btn page-link text-dark d-inline-block rounded-0">下書き</button>
+            </div>
         )
         return (
             <div>
-                <div class="flex-box-end">
-                    {saveButton}
+                <div className="flex-box-end">
+                    {saveButtons}
                 </div>
-                <div>
-                    {eleButtons}
-                </div>
-                <div>
-                    {inlineButtons}
+                <div className="row">
+                    <div className="col-md-6">
+                        <div>
+                            {elementButtons}
+                        </div>
+                        <div>
+                            {inlineButtons}
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="row">
+                            <div className="form-group col-md-6">
+                                <label htmlFor="exampleSelect1exampleFormControlSelect1">カテゴリの選択</label>
+                                <div className="row">
+                                    <input className="form-control col-md-6"></input>
+                                    <select className="form-control col-md-6" id="exampleFormControlSelect1">
+                                        <option>PHP</option>
+                                        <option>Ruby</option>
+                                        <option>JavaScript</option>
+                                        <option>環境構築</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="form-group col-md-6">
+                                <label htmlFor="exampleSelect1exampleFormControlSelect1">タグの選択</label>
+                                <div className="row">
+                                    <input className="form-control col-md-6"></input>
+                                    <select className="form-control col-md-6" id="exampleFormControlSelect1">
+                                        <option>Vue.js</option>
+                                        <option>React.js</option>
+                                        <option>Laravel</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <Editor
                     editorState={this.state.editorState}
